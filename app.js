a285663b32db1e7d5b0e980b98c066b48756ad76
mattiapuglisi099@gmail.com <mattiapuglisi099@gmail.com>
2022-03-05 14:15:31 +0100

    completo

---
+++ app.js
@@ -0,0 +1,15 @@
+
+const app = new Vue({
+    el : '#root',
+    data : {
+        email : [],
+    },
+    created() {
+        for(let i=0; i<10; i++){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then( ( res ) => {
+                this.email.push(res.data.response);
+            })
+        }
+    }
+})
